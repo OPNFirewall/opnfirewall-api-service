4e0ab593c1a5b9dbd32e4f3d1656f67d6487dfb2
Niklas Arnitz <niklas.arnitz@icloud.com>
2022-06-08 20:26:06 +0200

    Add Documentation to APIError

--- src/models/general/APIError.js
+++ src/models/general/APIError.js
@@ -1,3 +1,8 @@
+/**
+ * @class APIError
+ * @param errorcode The Error Code of the created Error
+ * @param description A brief description of the presented Error
+ */
 class APIError {
   constructor(errorcode, description) {
     this.errorcode = errorcode;
